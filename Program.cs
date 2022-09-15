0c4ed89a0460fb7fcf86b1d1a2daf1c5786982f0
sworc <lightbox@nifty.com>
2022-09-15 16:06:50 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace cs_0915_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = "コンピュータ 情報 専門 学校";
+            str = str.Replace("情報", "Information");
+
+            // ****************************
+            // ヒアドキュメント
+            // ****************************
+            string html = @"
+            <html>
+            <body>
+                <h1>$01</h1>
+            </body>
+            </html>";
+            html = html.Replace( "$01", str );
+            print(html);
+
+            // ****************************
+            // $ で埋め込み
+            // ****************************
+            html = $@"
+            <html>
+            <body>
+                <h1>{str}</h1>
+            </body>
+            </html>";
+            print(html);
+
+        }
+
+        private static void print(string v)
+        {
+            Console.WriteLine(v);
+        }
+    }
+}
